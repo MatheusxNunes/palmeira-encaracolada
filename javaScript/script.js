a509cbb20372ef6b248054553b3c86e0f3701382
Matheus Nunes <matheusfrancanunes13@gmail.com>
2026-06-02 20:15:51 -0300

    Atualizar o script.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,17 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => { 
-    const slider = document.getElementById('slider');
-    const antesWapper = document.querySelector('.img-anteswrapper');
-    const linha = document.querySelector('.linha-separadora');
+// 1. Pega os elementos do HTML que você já criou
+const slider = document.getElementById('slider');
+const imgAntesWrapper = document.querySelector('.img-antes-wrapper');
+const linhaSeparadora = document.querySelector('.linha-separadora');
 
-    if (!slider || !antesWapper || !linha ) {
-    console.error('Erro: não achei');
-    return;
-    }
-    console.log ('Comparador carregado');
-
-    slider.addEventListener('input', (e) => {
-        const valor = e.targety.value;
-        antesWapper.style.width = '%';
-        linha.style.left = '%';
-    });
+// 2. Escuta quando o usuário mexe no slider (input do tipo range)
+slider.addEventListener('input', (e) => {
+    const valor = e.target.value; // Pega o valor de 0 a 100
+    
+    // 3. Atualiza a largura da imagem de "antes" e a posição da linha dourada
+    imgAntesWrapper.style.width = `${valor}%`;
+    linhaSeparadora.style.left = `${valor}%`;
 });
